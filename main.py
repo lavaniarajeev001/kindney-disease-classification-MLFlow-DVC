55ae244cd34cc2b6199af4818fa60d1e29dd7073
lavaniarajeev001 <rajeev.lavania@yahoo.co.in>
2025-01-23 19:47:50 +0530

    prapare basic model module added

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.components.data_ingestion import Dataingestion
+from src.cnnClassifier.components.prepare_base_mode import PrepareBaseModel
 
 STAGE_NAME="Data Ingestion stage"
-
-try:
-    obj=DataIngestionPipeline()
-    obj.main()
+try: 
+   data_ingestion = DataIngestionPipeline()
+   data_ingestion.main()
 except Exception as e:
-    raise e
+        raise e
+
+
+
+STAGE_NAME = "Prepare base model stage"
+try: 
+   prepare_base_model = PrepareBaseModelPipeline()
+   prepare_base_model.main()
+except Exception as e:
+        raise e
 
     
---
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,26 @@
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.components.prepare_base_mode import PrepareBaseModel
+
+
+STAGE_NAME="Prepare base model stage"
+
+class PrepareBaseModelPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            prepare_base_model_config = config.get_prepare_base_model_config()
+            prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+            prepare_base_model.get_base_model()
+            prepare_base_model.update_base_model()
+        except Exception as e:
+            raise e
+        
+
+if __name__=="__main__":
+    try:
+        obj=PrepareBaseModelPipeline
+        obj.main()
+    except Exception as e:
+        raise e
